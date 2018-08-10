2291c975e0215871fd9bef72c9f42dc89e5c52ef
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2018-08-10 16:06:41 +0700

    setup project

---
+++ src/main/java/com/github/ipan97/springcloudstreamkafkaexample/SpringCloudStreamKafkaExampleApplication.java
@@ -0,0 +1,12 @@
+package com.github.ipan97.springcloudstreamkafkaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamKafkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamKafkaExampleApplication.class, args);
+	}
+}
